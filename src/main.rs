843e708eb3397f131cc8efdc64e79c6fb6cbab5f
Bruno Flores <bruno@brunoflores.com.br>
2021-11-05 16:05:56 +1000

    Loop and then exit.

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,22 @@ fn main() {
     let secret_number: u32 = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input a number");
-    let guess = ask();
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input a number");
+        let guess = ask();
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Nailed it"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Nailed it");
+                break;
+            }
+        }
     }
+
+    println!("Exiting now");
 }
 
 fn ask() -> u32 {
