20ee71e96b286c43dca96fe67bf5998926074a3c
Bruno Flores <bruno@brunoflores.com.br>
2021-11-05 15:58:38 +1000

    Compare.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess!");
+
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number);
+
     println!("Please input a number");
+    let guess = ask();
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("Nailed it"),
+    }
+}
+
+fn ask() -> u32 {
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    guess.trim().parse().expect("Please type a number")
 }
